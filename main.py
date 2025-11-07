af0dbccc4a2445fbcc71f180ef46a5a92b3ec076
nicku2026 <ungnick8@gmail.com>
2025-11-07 12:24:35 -0600

    Refactor food delivery service code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ print("Welcome to the food divery service   !")
 # Get user details
 name=input("What's your name: ")
 age=input("Hello "+name+"  How old are you: ")
+
+#Give time fo
 def choose_time():
     print("Choose time:")
     print("1: 12:00 PM")
@@ -15,6 +17,9 @@ if choice == ("1","2","3"):
 else:
     print ("Synatax Error")
 return choice
+time_choice = choose_time()
+
+# Order process
 menu = {
     1: ("Big Burger", 7.99),
     2: ("Chicken Sandwich", 6.49),
@@ -24,11 +29,36 @@ menu = {
 }
 while True:
 choice = int(input("Enter the number of the item you'd like to order: "))
+if choice == "0":
+        break
+    elif choice == "1":
+        order_list.append("Big Burger")
+        total += 7.99
+    elif choice == "2":
+        order_list.append("Chicken Sandwich")
+        total += 6.49
+    elif choice == "3":
+        order_list.append("Fries")
+        total += 2.99
+    elif choice == "4":
+        order_list.append("Soda")
+        total += 1.49
+    elif choice == "5":
+        order_list.append("Ice Cream")
+        total += 3.49
+    else:
+        print("Invalid choice. Please try again.")
 
-
+#Pickup method
 print("Would you like your order to be:")
 print("1. Delivered")
 print("2. Picked up")
 
 method = input("Enter 1 or 2: ")
+
+if method == "1":
+    delivery_method = "Delivery"
+else:
+    delivery_method = "Pickup"
+# Summary
 
